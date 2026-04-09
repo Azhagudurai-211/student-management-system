a542b848d48e1cd9bb9cff55f603a2c0d8e90142
Azhagudurai-211 <sazhagu211@gmail.com>
2026-04-09 16:52:32 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,3 @@
-const API_BASE = (
-  window.APP_CONFIG &&
-  window.APP_CONFIG.API_BASE_URL &&
-  !window.APP_CONFIG.API_BASE_URL.includes("replace-with-your-render-url")
-)
-  ? window.APP_CONFIG.API_BASE_URL.replace(/\/$/, '')
-  : "https://student-management-system-5s3e.onrender.com";
+window.APP_CONFIG = {
+API_BASE_URL: "https://student-management-system-5s3e.onrender.com"
+};
