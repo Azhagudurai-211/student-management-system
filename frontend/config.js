cfb08d2f68a754f610e89dfcc02b039d60fcc000
Azhagudurai-211 <sazhagu211@gmail.com>
2026-04-09 14:50:29 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,3 @@
-
 window.APP_CONFIG = {
-  // Paste your Render backend URL below after deployment.
-  // Example: "https://student-management-backend.onrender.com"
-  API_BASE_URL: "https://REPLACE-WITH-YOUR-RENDER-URL.onrender.com"
+  API_BASE_URL: "https://student-management-system-5s3e.onrender.com"
 };
